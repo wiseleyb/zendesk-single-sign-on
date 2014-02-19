a1633c78fe35eea84d38a0773442ac402af0ac5b
Ben Wiseley <wiseleyb@gmail.com>
2014-02-19 14:31:46 -0800

    minor change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
   def title(value)
     unless value.nil?
-      @title = "#{value} | ZendeskSingleSignOn"      
+      @title = "#{value} | ZendeskSingleSignOn"
+    end
+  end
+
+  def zendesk_help_url
+    if current_user
+      Zendesk.sign_into_zendesk(current_user)
+    else
+      'https://causesstaging.zendesk.com/hc/en-us/requests/new'
     end
   end
 end
